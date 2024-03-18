abff561f2c1b6bf48c26790e383de5bb4975a1ee
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-18 09:25:05 +0300

    Initial commit

---
+++ src/main/java/com/colak/springflywaytutorial/SpringFlywayTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.colak.springflywaytutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlywayTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFlywayTutorialApplication.class, args);
+	}
+
+}
